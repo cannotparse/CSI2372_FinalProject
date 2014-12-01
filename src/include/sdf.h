7ad058a53808df10ca29b35a08c462348edd967c
Rodger <rwald@uocsclub.ca>
2014-12-01 07:40:14 -0500

    fuck it

---
+++ src/include/sdf.h
@@ -0,0 +1,16 @@
+#ifndef SDF_H
+#define SDF_H
+
+
+class sdf
+{
+    public:
+        /** Default constructor */
+        sdf();
+        /** Default destructor */
+        virtual ~sdf();
+    protected:
+    private:
+};
+
+#endif // SDF_H
